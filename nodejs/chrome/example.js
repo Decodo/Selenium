7cf8e283b78b1ff02a27f1f76c1ff8707080f895
Žilvinas <159907476+sakanamk@users.noreply.github.com>
2025-04-22 15:00:54 +0300

    Update example.js

--- nodejs/chrome/example.js
+++ nodejs/chrome/example.js
@@ -3,7 +3,7 @@ require('chromedriver');
 var webdriver = require('selenium-webdriver'), By = webdriver.By,
     until = webdriver.until,  chrome = require("selenium-webdriver/chrome");  //Configures the Chromedriver
 
-let addr = 'gate.smartproxy.com:7000'                                         //Proxy host:port configuration
+let addr = 'gate.decodo.com:7000'                                         //Proxy host:port configuration
 let opt = new chrome.Options().addArguments(`--proxy-server=http://${addr}`)
 
 var driver = new webdriver.Builder()                                          //Initializes the configured Chromedriver
@@ -11,7 +11,7 @@ var driver = new webdriver.Builder()                                          //
  .setChromeOptions(opt)
  .build();
 
-driver.get('http://ip.smartproxy.com/')                                       //Target website
+driver.get('http://ip.decodo.com/')                                       //Target website
 
 var textPromise = driver.findElement(webdriver.By.css('body')).getText();     //Select desired Element from your target website
 textPromise.then((text) => {
