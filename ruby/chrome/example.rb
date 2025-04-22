ae27ace705acae367eee224c401171e64e824f26
Žilvinas <159907476+sakanamk@users.noreply.github.com>
2025-04-22 15:06:18 +0300

    Update example.rb

--- ruby/chrome/example.rb
+++ ruby/chrome/example.rb
@@ -1,10 +1,10 @@
 require 'rubygems'
 require 'selenium-webdriver'
 
-proxy = Selenium::WebDriver::Proxy.new(:http => "gate.smartproxy.com:7000")
+proxy = Selenium::WebDriver::Proxy.new(:http => "gate.decodo.com:7000")
 
 driver = Selenium::WebDriver.for :chrome, :proxy => proxy
-driver.navigate.to "http://ip.smartproxy.com/"
+driver.navigate.to "http://ip.decodo.com/"
 element = driver.find_element(:css, 'body')
 
 puts element.text
