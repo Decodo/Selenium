cffc513e3eaf40f94f3f1f4428fc466a6ba4575d
Žilvinas <159907476+sakanamk@users.noreply.github.com>
2025-04-22 15:06:36 +0300

    Update example.rb

--- ruby/firefox/example.rb
+++ ruby/firefox/example.rb
@@ -2,11 +2,11 @@ require 'rubygems'
 require 'selenium-webdriver'
 
 profile = Selenium::WebDriver::Firefox::Profile.new
-proxy = Selenium::WebDriver::Proxy.new(http: "gate.smartproxy.com:7000")
+proxy = Selenium::WebDriver::Proxy.new(http: "gate.decodo.com:7000")
 profile.proxy = proxy
 options = Selenium::WebDriver::Firefox::Options.new(profile: profile)
 driver = Selenium::WebDriver.for :firefox, options: options
-driver.navigate.to "http://ip.smartproxy.com/"
+driver.navigate.to "http://ip.decodo.com/"
 element = driver.find_element(:css, 'body')
 
 puts element.text
